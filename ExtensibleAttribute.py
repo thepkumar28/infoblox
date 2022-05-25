3afb365bf6b2279e35322a9fefad4f221133c843
Pratham Kumar <thepratham81@gmail..com>
2022-05-25 14:24:15 +1000

    File edited

--- ExtensibleAttribute.py
+++ ExtensibleAttribute.py
@@ -59,7 +59,7 @@ for v in search_network.values():
 print("Value : ")
 print(tmp)
 print("Extensible Attributes")
-print(tmp[1][2])
+print(tmp[1])
 #search_host = search_extensible_attribute(connection, "record:host", "Country", "Australia")
 ## Print the output:
 #print("Below is the Search Host :")
