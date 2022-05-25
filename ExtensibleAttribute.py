fefef66b157b5ef8eca898c5ffb9a4b6afc4d46c
Pratham Kumar <thepratham81@gmail..com>
2022-05-25 16:56:21 +1000

    File edited

--- ExtensibleAttribute.py
+++ ExtensibleAttribute.py
@@ -62,7 +62,7 @@ print("list size of value is:", len(tmp))
 extattrs = tmp[1][0]['extattrs']
 print("Extensible Attributes :\n", extattrs)
 print("list size of Extensible Attributes is:", len(extattrs))
-print("Description is :\n", tmp[1][0]['extattrs']['Description'])
+print("Description is :\n", tmp[1][0]['extattrs']['Description']['value'])
 #print(tmp[1][0]['network'])
 #tmp1 = tmp[1]
 #print(tmp1[0]['extattrs'])
