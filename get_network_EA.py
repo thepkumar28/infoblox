9d00110574fa599f297af48499c4c5e57b3e6b51
Pratham Kumar <thepratham81@gmail..com>
2022-05-26 14:42:55 +1000

    File edited

--- get_network_EA.py
+++ get_network_EA.py
@@ -20,7 +20,7 @@ def get_network_EA(connection, place_to_check: str):
     my_args = [
         place_to_check,
         {
-            'net': '10.1.0.0/16'
+            'networkview': '10.1.0.0/16'
         }
     ]
     kwargs = {
