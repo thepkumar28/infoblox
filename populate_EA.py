8757ba5749030afe1822e54842ed95bafa1db786
Pratham Kumar <thepratham81@gmail..com>
2022-05-30 18:09:36 +1000

    File edited

--- populate_EA.py
+++ populate_EA.py
@@ -10,8 +10,8 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 from infoblox_client.connector import Connector
 from infoblox_client import objects
-from infoblox_client import object_manager
-from infoblox_client.object_manager import InfobloxObjectManager
+#from infoblox_client import object_manager
+#from infoblox_client.object_manager import InfobloxObjectManager
 
 def default_infoblox_connection():
     opts = {'host': 'ipam.woolworths.com.au', 'wapi_version':'2.10', 'username': sys.argv[1], 'password': sys.argv[2]}
@@ -19,13 +19,14 @@ def default_infoblox_connection():
     return conn
 connection = default_infoblox_connection()
 ib_network = objects.Network.search(connection, network='131.226.217.128/27', network_view='default', return_fields=['network', 'extattrs'])
-#ib_network = objects.Network.search(connection, network='131.226.217.128/27', network_view='default', return_fields=['network', 'extattrs'])
+ib_network_container = objects.NetworkContainer.search(connection, network='131.226.192.0/18', network_view='default', return_fields=['network', 'extattrs'])
 ea = objects.EA({'Description': 'This is my test description...'})
 
 print("ib_network :\n", ib_network)
+print("ib_network_container :\n", ib_network_container)
 print("EA :\n", ea)
-ib_network.extattrs = ea
-ib_network.update()
+#ib_network.extattrs = ea
+#ib_network.update()
 #populate_EA_values = InfobloxObjectManager.update_network_options("131.226.192.0/18", ib_network.extattrs)
 
 #print("Below are the Network Container Values :\n" , populate_EA_values)
