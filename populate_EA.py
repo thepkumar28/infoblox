539767f9f3e2129d247024e911d38ece0db46aa8
Pratham Kumar <thepratham81@gmail..com>
2022-05-30 16:52:42 +1000

    File edited

--- populate_EA.py
+++ populate_EA.py
@@ -10,8 +10,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 from infoblox_client.connector import Connector
 from infoblox_client import objects
-from infoblox_client import object_manager as obm
-from obm import *
+from infoblox_client.object_manager import *
 
 def default_infoblox_connection():
     opts = {'host': 'ipam.woolworths.com.au', 'wapi_version':'2.10', 'username': sys.argv[1], 'password': sys.argv[2]}
