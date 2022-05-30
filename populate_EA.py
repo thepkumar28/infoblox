45b070794cacdd97636f1ac8f43d14e3dab290c7
Pratham Kumar <thepratham81@gmail..com>
2022-05-30 17:01:19 +1000

    File edited

--- populate_EA.py
+++ populate_EA.py
@@ -10,7 +10,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 from infoblox_client.connector import Connector
 from infoblox_client import objects
-from infoblox_client import object_manager
+from infoblox_client.object_manager import InfobloxObjectManager
 
 def default_infoblox_connection():
     opts = {'host': 'ipam.woolworths.com.au', 'wapi_version':'2.10', 'username': sys.argv[1], 'password': sys.argv[2]}
@@ -18,7 +18,7 @@ def default_infoblox_connection():
     return conn
 
 connection = default_infoblox_connection()
-ea = objects.EA({'Description': 'This is my test description'})
-populate_EA_values = InfobloxObjectManager.update_network_options(connection, "131.226.192.0/18", ea)
+extattrs = objects.EA({'Description': 'This is my test description'})
+populate_EA_values = InfobloxObjectManager.update_network_options(connection, "131.226.192.0/18", extattrs)
 
 print("Below are the Network Container Values :\n" , populate_EA_values)
