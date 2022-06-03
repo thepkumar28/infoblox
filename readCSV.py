7cda96f17112ca02f72d1aedf22dc01bb7a6da83
Pratham Kumar <thepratham81@gmail..com>
2022-06-03 18:10:37 +1000

    File edited

--- readCSV.py
+++ readCSV.py
@@ -8,19 +8,6 @@ with open('NetworkContainer_EA.csv', newline='') as csv_file:
         tmp = (dict(row))
         NC = tmp["Network Container"]
         del tmp["Network Container"]
-        print(tmp)
         print (NC)
-
-#with open('NetworkContainer_EA.csv') as csv_file:
-#    csv_reader = csv.reader(csv_file, delimiter=',')
-#    line_count = 0
-#    for row in csv_reader:
-#        if line_count == 0:
-#            #print(f'{", ".join(row)}')
-#            print(row[0])
-#            line_count += 1
-#        else:
-#            #print(f'{", ".join(row)}')
-#            print(row[0])
-#            line_count += 1
-#    print(f'Processed {line_count} lines.')
+        print(tmp)
+        
