4aeb646145a748824dc7c3f0bf80d662c9485531
Pratham Kumar <thepratham81@gmail..com>
2022-06-03 16:49:07 +1000

    File edited

--- readCSV.py
+++ readCSV.py
@@ -9,11 +9,11 @@ with open('NetworkContainer_EA.csv', newline='') as csv_file:
     for col in csv_reader:
         print (col)
 
-        #for row in col:
-#
-        #    #result[row['Network Container']] = row['Description']
-        #    #print (result)
-        #    print (row)
+        for row in col:
+
+            #result[row['Network Container']] = row['Description']
+            #print (result)
+            print (row)
 
 
 
