c6ec7a038ab7c2fe04b48e1cee1242fff342b078
Pratham Kumar <thepratham81@gmail..com>
2022-06-03 10:30:10 +1000

    File edited

--- readCSV.py
+++ readCSV.py
@@ -7,7 +7,7 @@ with open('NetworkContainer_EA.csv') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
+            print(f'{", ".join(row)}')
             line_count += 1
         #else:
         #    print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}')
