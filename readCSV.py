aec81e399480224ea5baae92f3b3bd71aa374516
Pratham Kumar <thepratham81@gmail..com>
2022-06-03 10:37:10 +1000

    File edited

--- readCSV.py
+++ readCSV.py
@@ -7,9 +7,11 @@ with open('NetworkContainer_EA.csv') as csv_file:
     line_count = 0
     for row in csv_reader:
         if line_count == 0:
-            print(f'{", ".join(row)}')
+            #print(f'{", ".join(row)}')
+            print({row[0]})
             line_count += 1
         else:
-            print(f'{", ".join(row)}')
+            #print(f'{", ".join(row)}')
+            print({row[0]})
             line_count += 1
     print(f'Processed {line_count} lines.')
