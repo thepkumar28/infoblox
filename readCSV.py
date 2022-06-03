858960312338ae124bfaf415eaf990ee731d3046
Pratham Kumar <thepratham81@gmail..com>
2022-06-03 11:39:21 +1000

    File edited

--- readCSV.py
+++ readCSV.py
@@ -4,14 +4,20 @@ import csv
 
 with open('NetworkContainer_EA.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count1 = 0
-    for row in csv_reader:
-        if line_count1 == 0:
-            #print(f'{", ".join(row)}')
-            print(row[0])
-            line_count1 += 1
-        else:
-            #print(f'{", ".join(row)}')
-            print(row[0])
-            line_count1 += 1
-    print(f'Processed {line_count1} lines.')
+    for row , col in csv_file:
+        print(row)
+        print(col)
+
+#with open('NetworkContainer_EA.csv') as csv_file:
+#    csv_reader = csv.reader(csv_file, delimiter=',')
+#    line_count = 0
+#    for row in csv_reader:
+#        if line_count == 0:
+#            #print(f'{", ".join(row)}')
+#            print(row[0])
+#            line_count += 1
+#        else:
+#            #print(f'{", ".join(row)}')
+#            print(row[0])
+#            line_count += 1
+#    print(f'Processed {line_count} lines.')
