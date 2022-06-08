d0b30ebd0bc09356bdddbdbca07738aed71b993f
Pratham Kumar <thepratham81@gmail..com>
2022-06-08 10:17:44 +1000

    File edited

--- populate_NetworkContainer_EA.py
+++ populate_NetworkContainer_EA.py
@@ -21,9 +21,10 @@ def populate_NetworkContainer_ExtensibleAttribute(nw=str, exatt=str):
     ib_network_container = objects.NetworkContainer.search(connection, network=nw, network_view='default', return_fields=['default', 'extattrs'])
     ea = objects.EA(exatt)
     ib_network_container.extattrs = ea
-    ib_network_container_update=ib_network_container.update()
-    return ib_network_container_update
+    ib_network_container.update()
+    #ib_network_container_update=ib_network_container.update()
+    #return ib_network_container_update
 
-populate_NetworkContainer_EA = populate_NetworkContainer_ExtensibleAttribute('131.226.192.0/18',{'Description': 'This is my test description'})
+populate_NetworkContainer_EA = populate_NetworkContainer_ExtensibleAttribute('131.226.192.0/18',{'Description': 'This is my test container description'})
 
 print("Populated Extensible Attribute for the Network Container is :\n" , populate_NetworkContainer_EA)
