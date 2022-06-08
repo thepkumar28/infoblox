1ce6d902b515cf296139972d63a367fc2c4fce90
Pratham Kumar <thepratham81@gmail..com>
2022-06-08 14:16:17 +1000

    File edited

--- convertDict.py
+++ convertDict.py
@@ -20,26 +20,26 @@ def convert_NetworkContainer_data_to_dict(nw=str):
     ib_network_container = objects.NetworkContainer.search(connection, network=nw, network_view='default', return_fields=['default', 'extattrs'])
     comment = ib_network_container.comment
     tmp = str(ib_network_container.extattrs)
-    Ea = tmp.strip("EAs:")
+    EA_NC = tmp.strip("EAs:")
     #using strip() and split()  methods
-    Ea_dict = dict((a.strip(), b.strip())  
+    EA_dict_NC = dict((a.strip(), b.strip())  
                      for a, b in (element.split('=')  
-                                  for element in Ea.split(',')))
-    return Ea_dict
+                                  for element in EA_NC.split(',')))
+    return EA_dict_NC
  
 def convert_Network_data_to_dict(nw=str):
     ib_network = objects.Network.search(connection, network=nw, network_view='default', return_fields=['default', 'extattrs'])
-    #comment = ib_network.comment
-    #tmp = str(ib_network.extattrs)
-    #Ea = tmp.strip("EAs:")
+    comment = ib_network.comment
+    tmp = str(ib_network.extattrs)
+    EA_N = tmp.strip("EAs:")
     #using strip() and split()  methods
-    #result = dict((a.strip(), b.strip())  
-    #                 for a, b in (element.split('=')  
-    #                              for element in Ea.split(',')))
-    return ib_network
+    EA_dict_N = dict((a.strip(), b.strip())  
+                     for a, b in (element.split('=')  
+                                  for element in EA_dict_N.split(',')))
+    return EA_dict_N
 
 converted_NetworkContainer_data = convert_NetworkContainer_data_to_dict('131.226.192.0/18') 
-#converted_Network_data = convert_Network_data_to_dict('131.226.217.128/27')  
+converted_Network_data = convert_Network_data_to_dict('131.226.217.128/27')  
 
-print(converted_NetworkContainer_data)
-#print(converted_Network_data)
+print('Network Container EA :\n', converted_NetworkContainer_data)
+print("Network EA :\n", converted_Network_data)
