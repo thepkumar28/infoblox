0c29c667dd3431c55dc054eaec81c72f995bec3b
Pratham Kumar <thepratham81@gmail..com>
2022-06-08 19:27:39 +1000

    File edited

--- populate_NetworkContainer_values_latest.py
+++ populate_NetworkContainer_values_latest.py
@@ -18,23 +18,19 @@ def default_infoblox_connection():
     return conn
 connection = default_infoblox_connection()
 
-def populate_NetworkContainer_ExtensibleAttribute(nw=str, comm=str, exatt=str):
+def populate_NetworkContainer_ExtensibleAttribute(nw=str, desc=str, exatt=str):
     ib_network_container = objects.NetworkContainer.search(connection, network=nw, network_view='default', return_fields=['default', 'extattrs', 'comment'])
     if ib_network_container.comment == None:
-        ib_network_container.comment = comm
+        ib_network_container.comment = desc
         ib_network_container.update()
     elif re.search("^SVR", ib_network_container.comment):
-        print('Comment is :', ib_network_container.comment)
-    else:
-        print('Comment is :', ib_network_container.comment)    
-    #ib_network_container.comment = comm
-    #ea = objects.EA(exatt)
-    #ib_network_container.extattrs = ea
-    #ib_network_container.update()
-    #ib_network_container_update=ib_network_container.update()
-    #return ib_network_container_update
-    #print(ib_network_container)
-#populate_NetworkContainer_ExtensibleAttribute('131.226.192.0/18', 'Dev Network Container Used for testing scripts as part of the IPAM Project', {'Description': 'This is my test container description', 'Environment': 'Test'})
+        ib_network_container.comment = desc
+        ib_network_container.update()
+        print('Comment is :', ib_network_container.comment)   
+    ea = objects.EA(exatt)
+    ib_network_container.extattrs = ea
+    ib_network_container.update()
+    print(ib_network_container)
 
 with open('NetworkContainer_EA.csv', newline='') as csv_file:
     csv_reader = csv.DictReader(csv_file)
@@ -47,3 +43,4 @@ with open('NetworkContainer_EA.csv', newline='') as csv_file:
         print(NC)
         print(Desc)
         print(EA_dict_NC)
+        populate_NetworkContainer_ExtensibleAttribute(NC,Desc,EA_dict_NC)
