2df2b638bf7a3fa1067c411d095feca133871237
Pratham Kumar <thepratham81@gmail..com>
2022-06-08 14:25:17 +1000

    File edited

--- get_Network_values.py
+++ get_Network_values.py
@@ -1,7 +1,6 @@
 #! /usr/bin/python3
 
 import sys
-
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -9,7 +8,7 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 from infoblox_client.connector import Connector
-
+from infoblox_client import objects
 
 def default_infoblox_connection():
     opts = {'host': 'ipam.woolworths.com.au', 'wapi_version':'2.10', 'username': sys.argv[1], 'password': sys.argv[2]}
@@ -17,24 +16,16 @@ def default_infoblox_connection():
     return conn
 connection = default_infoblox_connection()
 
-def get_network_values(connection, place_to_check: str, network: str):
-    my_args = [
-        place_to_check,
-        {
-            'network': network
-        }
-    ]
-    kwargs = {
-        'return_fields': [
-            'default',
-            'extattrs',
-        ]
-    }
-    result = connection.get_object(*my_args, **kwargs)
-    return result
+def get_network_values(nw=str):
+    ib_network = objects.Network.search(connection, network=nw, network_view='default', return_fields=['default', 'extattrs'])
+    comment = ib_network.comment
+    tmp = str(ib_network.extattrs)
+    EA_N = tmp.strip("EAs:")
+    #using strip() and split()  methods
+    EA_dict_N = dict((a.strip(), b.strip())  
+                     for a, b in (element.split('=')  
+                                  for element in EA_N.split(',')))
+    return EA_dict_N , comment
 
-get_Network_values = get_network_values(connection, "network", "131.226.217.128/27")
-
-print("Below are the Network Values :\n" , get_Network_values)
-
-#print(type(get_Network_values))
+converted_Network_data = get_network_values('131.226.217.128/27')
+print("Network Values :\n", converted_Network_data)
