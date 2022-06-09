b60ffa6996c17606fbb9bbf004d2292510e967dc
Pratham Kumar <thepratham81@gmail..com>
2022-06-09 22:26:07 +1000

    File edited

--- NetworkContainer.py
+++ NetworkContainer.py
@@ -21,8 +21,9 @@ def NetworkContainer_Attribute(nw=str, comm=str, exatt=str):
     ib_network_container = objects.NetworkContainer.search(connection, network=nw, network_view='default', return_fields=['default', 'extattrs'])
     #ib_network_container.comment = comm
     ea_dict = ib_network_container.extattrs.ea_dict
-    ea_dict.update(exatt.ea_dict)
-    merged_ea = objects.EA(ea_dict)
+    ea = objects.EA(exatt)
+    #ea_dict.update(exatt.ea_dict)
+    #merged_ea = objects.EA(ea_dict)
     #merged_ea = ea
     #ea = objects.EA(exatt)
     #ea_ex = ib_network_container.extattrs
@@ -32,12 +33,12 @@ def NetworkContainer_Attribute(nw=str, comm=str, exatt=str):
     #ib_network_container_update=ib_network_container.update()
     #return ib_network_container_update
     #print ("Searched :\n", ib_network_container)
-    #print ("EA Input :\n", ea)
+    print ("EA Input :\n", ea)
     #print ("latest EA :\n", ea_la)
     #print ("EA post assigment:\n", ib_network_container.extattrs)
-    print("Existing EA dictionary :\n", ea_dict)
+    #print("Existing EA dictionary :\n", ea_dict)
     #print ("Converted EA :\n", ea)
-    print("Merged EA Dictionary:\n", merged_ea)
+    #print("Merged EA Dictionary:\n", merged_ea)
 
 NetworkContainer_Attribute('131.226.192.0/18', 'Dev Network Container Used for testing scripts as part of the IPAM Project', {'Description': 'Dev Network Container Used for testing scripts as part of the IPAM Project'})
 
