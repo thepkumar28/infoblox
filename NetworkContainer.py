a1bb6f5f17190f1762316811641917f74cf1a9ed
Pratham Kumar <thepratham81@gmail..com>
2022-06-09 11:15:17 +1000

    File edited

--- NetworkContainer.py
+++ NetworkContainer.py
@@ -26,7 +26,8 @@ def NetworkContainer_Attribute(nw=str, comm=str, exatt=str):
     #ib_network_container_update=ib_network_container.update()
     #return ib_network_container_update
     print ("Searched :\n", ib_network_container)
-    print ("EA :\n", ea)
+    print ("Existing EA :\n", ib_network_container.extattrs)
+    print ("EA Input :\n", ea)
 
 NetworkContainer_Attribute('131.226.192.0/18', 'Dev Network Container Used for testing scripts as part of the IPAM Project', {'Description': 'This is my test container description', 'Environment': 'Test'})
 
