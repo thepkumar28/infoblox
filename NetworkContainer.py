a30f29f24fc946b9f7450e12589a2b395d33a0c2
Pratham Kumar <thepratham81@gmail..com>
2022-06-14 12:05:22 +1000

    File edited

--- NetworkContainer.py
+++ NetworkContainer.py
@@ -21,7 +21,7 @@ def NetworkContainer_Attribute(nw=str, comm=str, exatt=str):
     ib_network_container = objects.NetworkContainer.search(connection, network=nw, network_view='default', return_fields=['default', 'extattrs'])
     #ib_network_container.comment = comm
     ea_ex_dict = ib_network_container.extattrs.ea_dict
-    ea_ex_dict_desc = ib_network_container.extattrs.ea_dict.description
+    ea_ex_dict_desc = ib_network_container.extattrs.description
     print("Exisiting Description is :\n",ea_ex_dict_desc)
     #if ea_ex_dict['Description'] == " " :
     #    print("No Exisiting Description") 
