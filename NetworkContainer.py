be13bc926a094066783ec556aba640c932b0c1f0
Pratham Kumar <thepratham81@gmail..com>
2022-06-14 10:35:15 +1000

    File edited

--- NetworkContainer.py
+++ NetworkContainer.py
@@ -21,11 +21,15 @@ def NetworkContainer_Attribute(nw=str, comm=str, exatt=str):
     ib_network_container = objects.NetworkContainer.search(connection, network=nw, network_view='default', return_fields=['default', 'extattrs'])
     #ib_network_container.comment = comm
     ea_ex_dict = ib_network_container.extattrs.ea_dict
-    ea_dict = ib_network_container.extattrs.ea_dict
-    ea_in = objects.EA(exatt)
-    ea_ex = objects.EA(ea_dict)
-    ea_dict.update(exatt)
-    merged_ea = objects.EA(ea_dict)
+    desc = ea_ex_dict['Description']
+    #if ib_network_container.comment == None:
+    #    ib_network_container.comment = desc
+    #    ib_network_container.update()
+    #ea_dict = ib_network_container.extattrs.ea_dict
+    #ea_in = objects.EA(exatt)
+    #ea_ex = objects.EA(ea_dict)
+    #ea_dict.update(exatt)
+    #merged_ea = objects.EA(ea_dict)
     #merged_ea = ea
     #ea = objects.EA(exatt)
     #ea_ex = ib_network_container.extattrs
@@ -35,15 +39,16 @@ def NetworkContainer_Attribute(nw=str, comm=str, exatt=str):
     #ib_network_container_update=ib_network_container.update()
     #return ib_network_container_update
     #print ("Searched :\n", ib_network_container)
-    print("Existing EA dictionary :\n", ea_ex_dict)
-    print ("Existing EA type :\n", type(ea_ex_dict))
-    print ("EA Input object format :\n", ea_in)
-    print ("EA Input object format type :\n", type(ea_in))
-    print ("EA Existing object format :\n", ea_ex)
-    print ("EA Existing object format type :\n", type(ea_ex))
-    print ("EA Input :\n", exatt)
-    print ("EA Input type :\n", type(exatt))
-    print("Merged EA Dictionary:\n", ea_dict)
-    print("Merged EA :\n", merged_ea)
+    #print("Existing EA dictionary :\n", ea_ex_dict)
+    #print ("Existing EA type :\n", type(ea_ex_dict))
+    #print ("EA Input object format :\n", ea_in)
+    #print ("EA Input object format type :\n", type(ea_in))
+    #print ("EA Existing object format :\n", ea_ex)
+    #print ("EA Existing object format type :\n", type(ea_ex))
+    #print ("EA Input :\n", exatt)
+    #print ("EA Input type :\n", type(exatt))
+    #print("Merged EA Dictionary:\n", ea_dict)
+    #print("Merged EA :\n", merged_ea)
+    print("Exisitng description is:\n",desc)
 
 NetworkContainer_Attribute('131.226.192.0/18', 'Dev Network Container Used for testing scripts as part of the IPAM Project', {'Description': 'Dev Network Container Used for testing scripts as part of the IPAM Project'})
