03c36284ddd071291350912a5a04d3a8d2f1d6e9
Pratham Kumar <thepratham81@gmail..com>
2022-06-14 14:54:36 +1000

    File edited

--- NetworkContainer.py
+++ NetworkContainer.py
@@ -28,7 +28,6 @@ def NetworkContainer_Attribute(nw=str, comm=str, exatt=str):
         merged_ea = objects.EA(ea_ex_dict)
         ib_network_container.extattrs = merged_ea
         ib_network_container.update()
-        print("Updated EA dictionary is :\n", ea_ex_dict)
         desc = ea_ex_dict['Description']
         #print("The Description is:\n", desc)
         if ib_network_container.comment == None:
