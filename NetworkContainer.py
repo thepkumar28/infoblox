8b4885030fc67e6d2b68ee7136590e8c0bd669b8
Pratham Kumar <thepratham81@gmail..com>
2022-06-14 10:57:44 +1000

    File edited

--- NetworkContainer.py
+++ NetworkContainer.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 
 import sys
-
+import re
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -23,9 +23,12 @@ def NetworkContainer_Attribute(nw=str, comm=str, exatt=str):
     ea_ex_dict = ib_network_container.extattrs.ea_dict
     desc = ea_ex_dict['Description']
     RQ_num = ea_ex_dict['Request Number']
-    #if ib_network_container.comment == None:
-    #    ib_network_container.comment = desc
-    #    ib_network_container.update()
+    if ib_network_container.comment == None:
+        ib_network_container.comment = desc
+        print('Comment is:\n', ib_network_container.comment)
+        #ib_network_container.update()
+    elif re.search(r'^SVR',ib_network_container.comment,re.I):
+        print('Comment is:\n', ib_network_container.comment)
     #ea_dict = ib_network_container.extattrs.ea_dict
     #ea_in = objects.EA(exatt)
     #ea_ex = objects.EA(ea_dict)
@@ -51,6 +54,6 @@ def NetworkContainer_Attribute(nw=str, comm=str, exatt=str):
     #print("Merged EA Dictionary:\n", ea_dict)
     #print("Merged EA :\n", merged_ea)
     print("Exisitng description is:\n",desc)
-    print("Request Number is:\n",RQ_num)
+    #print("Request Number is:\n",RQ_num)
 
 NetworkContainer_Attribute('131.226.192.0/18', 'Dev Network Container Used for testing scripts as part of the IPAM Project', {'Description': 'Dev Network Container Used for testing scripts as part of the IPAM Project'})
