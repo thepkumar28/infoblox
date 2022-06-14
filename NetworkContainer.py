9133c479097c59f4753f585f9738f834b586eedf
Pratham Kumar <thepratham81@gmail..com>
2022-06-14 14:20:48 +1000

    File edited

--- NetworkContainer.py
+++ NetworkContainer.py
@@ -49,6 +49,24 @@ def NetworkContainer_Attribute(nw=str, comm=str, exatt=str):
             print("The latest comment is :\n", ib_network_container.comment)
 
     else:
-        print("The Description is:\n", ea_ex_dict["Description"])    
+        print("Description is Existing !!!")
+        desc = ea_ex_dict['Description']
+        #print("The Description is:\n", desc)
+        if ib_network_container.comment == None:
+            print("Comment field is Empty !!!")
+            ib_network_container.comment = desc
+            ib_network_container.update()
+            print("The updated comment is :\n", ib_network_container.comment)
+        elif re.search(r'^SVR|RFC',ib_network_container.comment,re.I):
+            print("The existing comment is as below and it needs to be updated:\n", ib_network_container.comment)
+            RQ_num = {'Request Number':ib_network_container.comment}
+            #print("Request number dict is :\n", RQ_num)
+            ea_ex_dict.update(RQ_num)
+            merged_ea = objects.EA(ea_ex_dict)
+            ib_network_container.extattrs = merged_ea
+            ib_network_container.comment = desc
+            ib_network_container.update()
+            print("The latest EA dictionary is :\n", ea_ex_dict)
+            print("The latest comment is :\n", ib_network_container.comment)    
           
 NetworkContainer_Attribute('131.226.192.0/18', 'Dev Network Container Used for testing scripts as part of the IPAM Project', {'Description': 'Dev Network Container Used for testing scripts as part of the IPAM Project'})
