22e884a0f94fc3e32162fe8a345acf677d428b61
Pratham Kumar <thepratham81@gmail..com>
2022-06-14 14:14:45 +1000

    File edited

--- NetworkContainer.py
+++ NetworkContainer.py
@@ -30,7 +30,7 @@ def NetworkContainer_Attribute(nw=str, comm=str, exatt=str):
         ib_network_container.update()
         print("Updated EA dictionary is :\n", ea_ex_dict)
         desc = ea_ex_dict['Description']
-        print("The Description is:\n", desc)
+        #print("The Description is:\n", desc)
         if ib_network_container.comment == None:
             print("Comment field is Empty !!!")
             ib_network_container.comment = desc
@@ -39,8 +39,7 @@ def NetworkContainer_Attribute(nw=str, comm=str, exatt=str):
         elif re.search(r'^SVR|RFC',ib_network_container.comment,re.I):
             print("The existing comment is as below and it needs to be updated:\n", ib_network_container.comment)
             RQ_num = {'Request Number':ib_network_container.comment}
-            print("Request number dict is :\n", RQ_num)
-            desc = ea_ex_dict['Description']
+            #print("Request number dict is :\n", RQ_num)
             ea_ex_dict.update(RQ_num)
             merged_ea = objects.EA(ea_ex_dict)
             ib_network_container.extattrs = merged_ea
@@ -52,40 +51,4 @@ def NetworkContainer_Attribute(nw=str, comm=str, exatt=str):
     else:
         print("The Description is:\n", ea_ex_dict["Description"])    
           
-       
-    #RQ_num = ea_ex_dict['Request Number']
-    #if ib_network_container.comment == None:
-    #    ib_network_container.comment = desc
-    #    print('Comment is:\n', ib_network_container.comment)
-    #    #ib_network_container.update()
-    #elif re.search(r'^SVR|RFC',ib_network_container.comment,re.I):
-#
-    #    print('Comment is:\n', ib_network_container.comment)
-    #ea_dict = ib_network_container.extattrs.ea_dict
-    #ea_in = objects.EA(exatt)
-    #ea_ex = objects.EA(ea_dict)
-    #ea_dict.update(exatt)
-    #merged_ea = objects.EA(ea_dict)
-    #merged_ea = ea
-    #ea = objects.EA(exatt)
-    #ea_ex = ib_network_container.extattrs
-    #ea_la = ib_network_container.ea
-    #ib_network_container.extattrs = ea
-    #ib_network_container.update()
-    #ib_network_container_update=ib_network_container.update()
-    #return ib_network_container_update
-    #print ("Searched :\n", ib_network_container)
-    #print("Existing EA dictionary :\n", ea_ex_dict)
-    #print ("Existing EA type :\n", type(ea_ex_dict))
-    #print ("EA Input object format :\n", ea_in)
-    #print ("EA Input object format type :\n", type(ea_in))
-    #print ("EA Existing object format :\n", ea_ex)
-    #print ("EA Existing object format type :\n", type(ea_ex))
-    #print ("EA Input :\n", exatt)
-    #print ("EA Input type :\n", type(exatt))
-    #print("Merged EA Dictionary:\n", ea_dict)
-    #print("Merged EA :\n", merged_ea)
-    #print("Exisitng description is:\n",desc)
-    #print("Request Number is:\n",RQ_num)
-
 NetworkContainer_Attribute('131.226.192.0/18', 'Dev Network Container Used for testing scripts as part of the IPAM Project', {'Description': 'Dev Network Container Used for testing scripts as part of the IPAM Project'})
