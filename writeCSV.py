5b0636b4666a758bc3f2647bed3f694eb5542a1b
Pratham Kumar <thepratham81@gmail..com>
2022-06-16 20:55:34 +1000

    File edited

--- writeCSV.py
+++ writeCSV.py
@@ -24,11 +24,15 @@ def write_to_CSV(nw=str):
     ea_ex_dict = ib_network_container.extattrs.ea_dict
     
     print("The EA Dictionary is :\n", ea_ex_dict)
+    
+    my_dict = {'Network Container':nw, 'Comment':ib_network_container.comment, 'Description':ea_ex_dict['Description']}
 
     with open('write.csv', 'w', newline='') as csv_file:
-        fieldnames = ['Country', 'Description', 'Environment', 'Operational State', 'Request Number']
+        #fieldnames = ['Country', 'Description', 'Environment', 'Operational State', 'Request Number']
+        fieldnames = ['Network Container', 'Comment', 'Description']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
-        writer.writerow(ea_ex_dict)
+        #writer.writerow(ea_ex_dict)
+        writer.writerow(my_dict)
 
 write_to_CSV('131.226.192.0/18')
