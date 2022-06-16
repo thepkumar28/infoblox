6a3bf72f68abb5feff21bddd39a138f1848ed71b
Pratham Kumar <thepratham81@gmail..com>
2022-06-16 21:41:36 +1000

    File edited

--- writeCSV.py
+++ writeCSV.py
@@ -17,10 +17,10 @@ def default_infoblox_connection():
     return conn
 connection = default_infoblox_connection()
 
-with open('write.csv', 'w', newline='') as csv_file:
-    fieldnames = ['Network Container', 'Comment', 'Description']
-    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-    writer.writeheader()
+#with open('write.csv', 'w', newline='') as csv_file:
+#    fieldnames = ['Network Container', 'Comment', 'Description']
+#    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+#    writer.writeheader()
 
 def write_to_CSV(nw=str):
 
@@ -31,6 +31,11 @@ def write_to_CSV(nw=str):
     print("The EA Dictionary is :\n", ea_ex_dict)
     
     my_dict = {'Network Container':nw, 'Comment':ib_network_container.comment, 'Description':ea_ex_dict['Description']}
+    
+    with open('write.csv', 'w', newline='') as csv_file:
+        fieldnames = ['Network Container', 'Comment', 'Description']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
 
     with open('write.csv', 'a', newline='') as csv_file:
         fieldnames = ['Network Container', 'Comment', 'Description']
