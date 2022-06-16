10494ce7bb288a0b2c2be8f78134c8d427c83d87
Pratham Kumar <thepratham81@gmail..com>
2022-06-17 00:03:05 +1000

    File edited

--- NetworkContainer.py
+++ NetworkContainer.py
@@ -104,9 +104,9 @@ with open('NetworkContainer.csv', newline='') as csv_file:
         for k,v in tmp.items():
             if k == "Network Container":
                 NC_dict = {k:v}
-                NC = NC_dict["Network Container"]
+                #NC = NC_dict["Network Container"]
             if k == "Description":
                 Desc_dict = {k:v}
             #NetworkContainer_Attribute(NC,Desc_dict)
-            print("NC :\n", NC)
+            print("NC_dict :\n", NC_dict)
             print("Desc_dict :\n", Desc_dict)
