fb3fba8ae99723c3061cdf4e269bb2a968aa3a35
Pratham Kumar <thepratham81@gmail..com>
2022-06-16 19:35:06 +1000

    File edited

--- writeCSV.py
+++ writeCSV.py
@@ -26,7 +26,7 @@ def write_to_CSV(nw=str):
     print("The EA Dictionary is :\n", ea_ex_dict)
 
     with open('write.csv', 'w', newline='') as csv_file:
-        fieldnames = ['Country', 'Descriptions', 'Environment', 'Operational State', 'Request Number']
+        fieldnames = ['Country', 'Description', 'Environment', 'Operational State', 'Request Number', 'xxx']
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
         writer.writerow(ea_ex_dict)
