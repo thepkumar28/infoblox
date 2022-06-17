6cda103df0b87f1647ed7f0209254b79f6e36010
Pratham Kumar <thepratham81@gmail..com>
2022-06-17 22:46:18 +1000

    File edited

--- check_file.py
+++ check_file.py
@@ -2,7 +2,7 @@
 
 import os.path
 
-if os.path.exists(backup_NetworkContainer_data.csv) == True:
+if os.path.exists('backup_NetworkContainer_data.csv') == True:
     print("backup_NetworkContainer_data.csv exists")
 else:
     print("backup_NetworkContainer_data.csv doesn't exist")    
