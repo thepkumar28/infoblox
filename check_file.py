9baf301268208e8035bee8346955db325bc13921
Pratham Kumar <thepratham81@gmail..com>
2022-06-17 22:45:07 +1000

    File edited

---
+++ check_file.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+
+import os.path
+
+if os.path.exists(backup_NetworkContainer_data.csv) == True:
+    print("backup_NetworkContainer_data.csv exists")
+else:
+    print("backup_NetworkContainer_data.csv doesn't exist")    
