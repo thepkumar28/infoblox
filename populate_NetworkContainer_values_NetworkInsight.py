7d254a7817c27980e473968d5d7b6cc1f99f8a91
Pratham Kumar <thepratham81@gmail..com>
2022-06-18 21:36:53 +1000

    File edited

--- populate_NetworkContainer_values_NetworkInsight.py
+++ populate_NetworkContainer_values_NetworkInsight.py
@@ -20,6 +20,7 @@ connection = default_infoblox_connection()
 def populate_NetworkContainer_ExtensibleAttribute(nw=str, desc=str, exatt=str):
     ib_network_container = objects.NetworkContainer.search(connection, network=nw, network_view='default', return_fields=['default', 'extattrs'])
     if ib_network_container.comment == None:
+        ib_network_container.unmanaged('false')
         ib_network_container.comment = desc
         ib_network_container.update()
 
