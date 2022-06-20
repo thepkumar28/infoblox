c356228edd285af197969691d3ae53a7c4600404
Pratham Kumar <thepratham81@gmail..com>
2022-06-20 11:48:16 +1000

    File edited

--- NetworkContainerPostIntervention.py
+++ NetworkContainerPostIntervention.py
@@ -24,16 +24,27 @@ if os.path.exists('UserIntervention.csv') == True:
     def NetworkContainer_Attribute_PostIntervention(nw=str, comm=str, desc=str):
         ib_network_container = objects.NetworkContainer.search(connection, network=nw, network_view='default', return_fields=['default', 'extattrs', 'unmanaged'])
         
+        ea_ex_dict = ib_network_container.extattrs.ea_dict
+
+        ib_network_container.comment = comm
+
+        ea_ex_dict.update(desc)
+        merged_ea = objects.EA(ea_ex_dict)
+        ib_network_container.extattrs = merged_ea
+        ib_network_container.update()
+
+        print("Updated the the values for {} as mentioned in UserIntervention.csv file".format(nw))
 
     with open('UserIntervention.csv', newline='') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         for row in csv_reader:
             tmp = (dict(row))
-            print(tmp)
-            #NC = tmp["Network Container"]
-            #description_key = ['Description']
-            #desc = {k:tmp[k] for k in description_key if k in tmp}
-            #NetworkContainer_Attribute_PostIntervention(NC,desc)    
+            #print(tmp)
+            NC = tmp["Network Container"]
+            comm = tmp["Comment"]
+            description_key = ['Description']
+            desc = {k:tmp[k] for k in description_key if k in tmp}
+            NetworkContainer_Attribute_PostIntervention(NC, comm, desc)  
 
 else:
     print("Please run NetworkContainer.py first , then execute this script !!!")    
