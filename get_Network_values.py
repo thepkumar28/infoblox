056460d77feba4db5d4d140602f0f118dd942026
Pratham Kumar <thepratham81@gmail..com>
2022-06-22 15:37:59 +1000

    File edited

--- get_Network_values.py
+++ get_Network_values.py
@@ -18,15 +18,16 @@ connection = default_infoblox_connection()
 
 def get_network_values(nw=str):
     ib_network = objects.Network.search(connection, network=nw, network_view='default', return_fields=['default', 'extattrs'])
-    comment = ib_network.comment
-    EA_dict_N = ib_network.extattrs.ea_dict
+    #comment = ib_network.comment
+    #EA_dict_N = ib_network.extattrs.ea_dict
     #tmp = str(ib_network.extattrs)
     #EA_N = tmp.strip("EAs:")
     ##using strip() and split()  methods
     #EA_dict_N = dict((a.strip(), b.strip())  
     #                 for a, b in (element.split('=')  
     #                              for element in EA_N.split(',')))
-    return EA_dict_N , comment
+    #return EA_dict_N , comment
+    return ib_network
 
 converted_Network_data = get_network_values('10.114.128.0/18')
 print("Network Values :\n", converted_Network_data)
