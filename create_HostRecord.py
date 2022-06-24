ffda2f6ec3763ee9caaf7f744655fd45335133d2
Pratham Kumar <thepratham81@gmail..com>
2022-06-24 12:28:28 +1000

    File edited

--- create_HostRecord.py
+++ create_HostRecord.py
@@ -10,6 +10,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 from infoblox_client.connector import Connector
 from infoblox_client import objects
+from netmiko import ConnectHandler
 
 def default_infoblox_connection():
     opts = {'host': 'ipam.woolworths.com.au', 'wapi_version':'2.10', 'username': sys.argv[1], 'password': sys.argv[2]}
@@ -17,4 +18,47 @@ def default_infoblox_connection():
     return conn
 connection = default_infoblox_connection()
 
-def create_Host():
+def connect_to_host():
+        host1 = {
+        "device_type": "cisco_ios",
+        "host": "sandbox-iosxr-1.cisco.com",
+        "username": "admin",
+        "password": "C1sco12345",
+    }
+        host2 = {
+        "device_type": "cisco_ios",
+        "host": "sandbox-nxos-1.cisco.com",
+        "username": "admin",
+        "password": "Admin_1234!",
+    }
+        host3 = {
+        "device_type": "cisco_ios",
+        "host": "sandbox-iosxe-latest-1.cisco.com",
+        "username": "developer",
+        "password": "C1sco12345",
+    }
+
+#def create_Host():
+
+host_list = [
+    host1 = {
+        "device_type": "cisco_ios",
+        "host": "sandbox-iosxr-1.cisco.com",
+        "username": "admin",
+        "password": "C1sco12345",
+    }
+    host2 = {
+        "device_type": "cisco_ios",
+        "host": "sandbox-nxos-1.cisco.com",
+        "username": "admin",
+        "password": "Admin_1234!",
+    }
+    host3 = {
+        "device_type": "cisco_ios",
+        "host": "sandbox-iosxe-latest-1.cisco.com",
+        "username": "developer",
+        "password": "C1sco12345",
+    }
+]
+
+print(host_list)
